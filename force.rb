0acb59a954a8f5060d67292d1cf25f077f5936b2
Taro Uraoka <turaoka@pasonatquila.com>
2017-01-04 22:51:00 +0900

    Update force cli to 0.22.64

--- force.rb
+++ force.rb
@@ -1,9 +1,9 @@
 class Force < Formula
   desc 'A command-line interface to force.com.'
   homepage 'https://force-cli.herokuapp.com/'
-  version '0.22.63'
+  version '0.22.64'
   url "https://s3-us-west-2.amazonaws.com/force-cli/heroku/force/v#{version}/darwin-amd64/force"
-  sha256 '4d9574c7236ac6b3b07a7014c7b99dca3be06e6a4af840268a91f2e6cffbee17'
+  sha256 'af92714ad4015bd70000e1ed5efcab657639b78178a20df63ba79286322935ce'
 
   bottle :unneeded
 
