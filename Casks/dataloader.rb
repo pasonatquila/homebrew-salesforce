ba44fca6eee9e551cf36efaf3036a49d699962b3
Taro Uraoka <turaoka@pasonatquila.com>
2018-03-15 18:13:36 +0900

    Update dataloader to V42.0

--- Casks/dataloader.rb
+++ Casks/dataloader.rb
@@ -1,6 +1,6 @@
 cask 'dataloader' do
-  version '41.0.0'
-  sha256 'e64db626a02c28108c4f28c682ebc7be03923493fbbf8120b51f753afb69a41a'
+  version 'V42.0'
+  sha256 '2dcc3d908bc316048abf7c4fe9681f1f8e415e6c2ee26f45749eb5e84bed70ce'
 
   url "https://github.com/forcedotcom/dataloader/releases/download/#{version}/ApexDataLoader.dmg"
   name 'Force.com Data Loader'
